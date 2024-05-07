bb8a3850333d86a3391b720f1dc9980c96057804
Ritesh-2001 <72019383+Ritesh-2001@users.noreply.github.com>
2024-05-07 12:38:58 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,97 @@
+##-- To run the streamlit app, run the following command in the terminal: streamlit run streamlit_app.py
+# pip install pandas selfcheckgpt sentence_transformers spacy streamlit torch
+
+import streamlit as st
+import utils
+import pandas as pd
+from time import time
+import re
+
+# Streamlit app layout
+st.title('Hallucination Mitigation')
+
+# Text input
+user_input = st.text_input("Enter your text:")
+#st.image('selfcheck_llm.png', caption='LLM prompted Score')     ###---- Comment out if no image
+
+
+if user_input:
+
+    prompt = user_input
+
+    output, sampled_passages = utils.get_output_and_samples(prompt)
+
+    # # NLI score
+    # self_similarity_score = utils.get_self_check_nli(output,sampled_passages)
+    # st.write(self_similarity_score)
+    # st.write("Actual output: ", output)
+    # st.write("**Sampled passages:**")
+    # for i, passage in enumerate(sampled_passages):
+    #     st.write(f"Sample {i+1}: {passage}")
+    # LLM score
+    
+    start=time()
+    self_similarity_score = utils.llm_evaluate2(output,sampled_passages) #use llm_evaluate or llm_evaluate2
+
+    print('self_similarity_score:',self_similarity_score)
+    print(type(self_similarity_score))
+    try:
+        self_similarity_score = float(self_similarity_score)
+    except ValueError:
+    # Extract number with decimal point using a capturing group
+        match = re.search(r'\d+\.\d+', self_similarity_score) 
+        if match:
+            self_similarity_score = float(match.group())  # Extract the entire matched group
+            # If llm_evaluate instead of llm_evaluate2 is used, then add line self_similarity_score=1-self_similarity_score
+        else:
+            # Handle cases where no number with decimal point is found (optional)
+            self_similarity_score = 0.5  # Or raise an error
+
+    end = time()
+
+    # Display the output
+    print(self_similarity_score)
+    st.write("Score:",self_similarity_score)
+    st.write("**LLM output:**")
+    threshold = 0.5
+    if self_similarity_score < threshold:           ####----- Change the threshold value as needed
+        st.write(output)
+        st.write("**Sampled passages:**")
+        for i, passage in enumerate(sampled_passages):
+            st.write(f"Sample {i+1}: {passage}")
+    else:
+        st.write(f"I'm sorry, but I don't have the specific information required to answer your question accurately. Self-similarity score {self_similarity_score} is above the threshold {threshold}.\nNOTE: score closer to 1 indicates higher chance of hallucination")
+        st.write("**Actual output:** ", output)
+        st.write("**Sampled passages:**")
+        for i, passage in enumerate(sampled_passages):
+            st.write(f"Sample {i+1}: {passage}")
+    # add image in streamlit
+    st.image('llm_prompted_Score.png', caption='LLM prompted Score')   ####----- Comment out if no image
+
+# Hallucination score = 1- self_similarity_score
+
+# # TESTING APP WITHOUT STREAMLIT (FOR DEBUGGING)
+# prompt='Who is Evelyn hartwell?'
+# output, sampled_passages = utils.get_output_and_samples(prompt)
+# print('output:',output)
+# print('sample:',sampled_passages)
+# # LLM score
+# start = time()
+# # self_similarity_score = utils.llm_evaluate(output,sampled_passages)
+# self_similarity_score,df = utils.get_self_check_nli(output,sampled_passages)
+
+# # try:
+# #   self_similarity_score = float(self_similarity_score)
+# # except ValueError:
+# #   # Extract number with decimal point using a capturing group
+# #   match = re.search(r'\d+\.\d+', self_similarity_score)
+# #   if match:
+# #     self_similarity_score = float(match.group())  # Extract the entire matched group
+# #   else:
+# #     # Handle cases where no number with decimal point is found (optional)
+# #     self_similarity_score = None  # Or raise an error
+
+# end = time()
+# print(type(self_similarity_score))
+# print('self_similarity_score:',self_similarity_score,'END')
+# print(df.head)
